b129838f8dd8d7d429c2de4dd8030eb250636483
Kabelo Hlako <55398494+hlakokabelo@users.noreply.github.com>
2026-05-13 16:01:24 +0200

    Added Vercel speed insights and analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { appName } from "./utils/appName.ts";
 
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 document.title = appName;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+    <Analytics />
+    <SpeedInsights />
+  </StrictMode>
 );
