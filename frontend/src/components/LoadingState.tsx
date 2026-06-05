70c33d7bd32f69ce07df0f771e0580a82620e42e
Kabelo Hlako <55398494+hlakokabelo@users.noreply.github.com>
2026-06-05 14:26:03 +0200

    Update LoadingState.tsx

--- frontend/src/components/LoadingState.tsx
+++ frontend/src/components/LoadingState.tsx
@@ -1,9 +1,18 @@
+import { useAuthStore } from "../stores/authStore";
+
+
 export function LoadingSate() {
+  const { isServerAwake } = useAuthStore();
   return (
     <div className=" min-h-screen flex flex-col items-center justify-center">
       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
 
       <p className="mt-6 text-black text-lg"></p>
+{!isServerAwake && (
+  <div>
+    Server is waking up... this may take a minute ☕
+  </div>
+)}
     </div>
   );
 }
