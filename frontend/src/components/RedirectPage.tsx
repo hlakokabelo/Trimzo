bbb0b629aadab873c8658c739c6f6df64c3eafdc
Kabelo Hlako <55398494+hlakokabelo@users.noreply.github.com>
2026-07-06 02:24:30 +0200

    Update RedirectPage.tsx

--- frontend/src/components/RedirectPage.tsx
+++ frontend/src/components/RedirectPage.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getUrl } from "../services/dbServices.ts";
+import { useAuthStore } from "../stores/authStore";
 
 interface IRedirectPageProps {}
 
 const RedirectPage: React.FunctionComponent<IRedirectPageProps> = () => {
   const { shortUrl } = useParams();
   const navigate = useNavigate();
+  const { isServerAwake } = useAuthStore();
   React.useEffect(() => {
     const run = async () => {
       const { data } = await getUrl(shortUrl!);
@@ -22,13 +24,13 @@ const RedirectPage: React.FunctionComponent<IRedirectPageProps> = () => {
   }, []);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
+    {isServerAwake &&<div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
 
       <p className="mt-6 text-black text-lg">
         Redirecting you to your destination...
       </p>
-    </div>
+    </div>} 
   );
 };
 
