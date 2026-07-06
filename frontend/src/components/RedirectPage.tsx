755a47ef049599e5c568fbc3e0fc393db7a3c051
Kabelo Hlako <55398494+hlakokabelo@users.noreply.github.com>
2026-07-06 14:27:32 +0200

    Fixed syntax error

--- frontend/src/components/RedirectPage.tsx
+++ frontend/src/components/RedirectPage.tsx
@@ -21,16 +21,18 @@ const RedirectPage: React.FunctionComponent<IRedirectPageProps> = () => {
     };
 
     run();
-  }, []);
+  }, [navigate, shortUrl]); 
 
   return (
-    {isServerAwake &&<div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
+   <>
+ {isServerAwake &&<div className="min-h-screen flex flex-col items-center justify-center bg-gray-300">
       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
 
       <p className="mt-6 text-black text-lg">
         Redirecting you to your destination...
       </p>
     </div>} 
+</>
   );
 };
 
